4879203ba22b305e31dbd9d0cf9d72533c9b8e73
karsyn ansari <karsyn.ansari@gmail.com>
2022-03-16 12:41:42 -0600

    commit tree 12f7588b934671aaad8f24fe52f94254c8eae61d

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,5 +4,11 @@ class MoviesController < ApplicationController
     movies = Movie.all
     render json: movies
   end
-
+def create
+    movie = Movi.create(movie_params)
+  render json: movie, status: :created
+end
+def movie_params
+  params.permit(:title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director)
+end
 end
